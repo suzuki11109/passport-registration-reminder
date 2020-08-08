9c8b908a273a516beac3129fd0237a42c2d48fbf
Suzuki Aki <suzuki11109@gmail.com>
2020-08-08 16:55:58 +0900

    set scheduler and send line message

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/suzuki11109/passport-registraion-reminder
 
 go 1.14
 
-require github.com/gocolly/colly/v2 v2.1.0 // indirect
+require (
+	github.com/gocolly/colly/v2 v2.1.0 // indirect
+	github.com/line/line-bot-sdk-go v7.5.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -1,30 +1,81 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gocolly/colly/v2"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-const word = "มีผู้ลงทะเบียนเต็มแล้ว"
+const (
+	formURL = "https://docs.google.com/forms/d/e/1FAIpQLSeGvkG2Eoe8d0FVNxxH5GL9HbXFUgysVTtK9XLs_4rYKqPXxQ/viewform"
+	word    = "มีผู้ลงทะเบียนเต็มแล้ว"
+)
 
 func main() {
-	var found bool
-	c := colly.NewCollector()
+	collector := colly.NewCollector()
 
-	c.OnRequest(func(r *colly.Request) {
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userID := os.Getenv("USER_ID")
+	if userID == "" {
+		log.Fatal(errors.New("user id is required"))
+	}
+
+	fmt.Println("starting...")
+
+	t := time.NewTicker(time.Duration(3 * time.Minute))
+	defer t.Stop()
+
+	go func() {
+		for t := range t.C {
+			fmt.Println("run at", t)
+			if !checkRegistrationForm(collector) {
+				messageMe(bot, userID, "Registration is ready! Let's do it right away")
+				messageMe(bot, userID, formURL)
+			} else {
+				fmt.Println("Still not ready for registration")
+			}
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	<-stop
+	fmt.Println("done")
+}
+
+func messageMe(bot *linebot.Client, userID, text string) {
+	message := linebot.NewTextMessage(text)
+
+	if _, err := bot.PushMessage(userID, message).Do(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func checkRegistrationForm(collector *colly.Collector) bool {
+	var found bool
+	collector.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.OnHTML(fmt.Sprintf("[data-value='%s']", word), func(e *colly.HTMLElement) {
+	collector.OnHTML(fmt.Sprintf("[data-value='%s']", word), func(e *colly.HTMLElement) {
 		found = true
 	})
 
-	c.Visit("https://docs.google.com/forms/d/e/1FAIpQLSeGvkG2Eoe8d0FVNxxH5GL9HbXFUgysVTtK9XLs_4rYKqPXxQ/viewform")
-
-	if !found {
-		fmt.Println("Registration is ready! Let's do it right away")
-	} else {
-		fmt.Println("Still not ready for registration")
-	}
+	collector.Visit(formURL)
+	return found
 }
